807281af5a57f31b59b369a964b23350a8c0d47b
Nicholas Shook <nick@1337lawyers.com>
2020-01-30 11:46:02 -0800

    basic form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,4 @@
 import Heading from '@kiwicom/orbit-components/lib/Heading';
-import InputField from '@kiwicom/orbit-components/lib/InputField';
 import React from 'react';
 import Text from '@kiwicom/orbit-components/lib/Text';
 import { useFormik } from 'formik';
@@ -27,34 +26,19 @@ const ContactRickiePage = () => {
         1250 E Arica Road
         Eloy, AZ 85131
       </Text>
+      <form
+        action="https://formspree.io/moqdvlav"
+        method="POST"
+      >
+        <Text>
 
-      <form onSubmit={formik.handleSubmit}>
-        <InputField
-          autoComplete="off"
-          dataTest="contact-email"
-          error={formik.errors.name}
-          id="ID"
-          label="Name"
-          name="name"
-          onBlur={formik.handleBlur}
-          onChange={formik.handleChange}
-          placeholder="Your Name"
-          type="text"
-          value={formik.values.name}
-        />
-        <InputField
-          autoComplete="off"
-          dataTest="contact-email"
-          error={formik.errors.name}
-          id="ID"
-          label="Email"
-          name="email"
-          onBlur={formik.handleBlur}
-          onChange={formik.handleChange}
-          placeholder="Your Email"
-          type="text"
-          value={formik.values.name}
-        />
+          Your email: <input type="text" name="_replyto" />
+        </Text>
+        <Text>
+          Your message: <textarea name="message" />
+        </Text>
+
+        <button type="submit">Send Information</button>
       </form>
     </>
   );
